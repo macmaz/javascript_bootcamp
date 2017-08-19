2e69673f6c8d8b55ad972238ba3fa397575b4371
macmaz <maciej.mazurek87@gmail.com>
2017-08-19 16:58:46 +0200

    retur result

--- js/function.js
+++ js/function.js
@@ -1,6 +1,7 @@
 function multiply(par1, par2, par3) {
     var result = par1 * par2 * par3;
-    console.log(result)
+    return result;
 } 
 
-multiply(3,5,9);
+var wynik = multiply(3,5,9);
+console.log(wynik);
