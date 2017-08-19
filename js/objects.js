8c66aeaeada8c7bf21a0dcb800d185e3aa02a2a4
macmaz <maciej.mazurek87@gmail.com>
2017-08-19 14:33:54 +0200

    object functions

--- js/objects.js
+++ js/objects.js
@@ -29,16 +29,21 @@ var kaja = {
 kaja.przedstawOsobe() */
 
 class Osoba {
-    constructor (imie,nazwisko,wzrost, oczy) {
-this.imie = imie;
-this.nazwisko = nazwisko;
+    constructor(imie, nazwisko, wzrost, oczy) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
         this.wzrost = wzrost;
         this.oczy = oczy;
+    }
+    
+    wyswietlInfo() {
+        console.log (this.imie + "\n" 
+            + this.nazwisko + "\n"
+            +this.wzrost + "\n"
+            +this.oczy)
+    }
 }
-}
 
-var maciej = new Osoba('Maciej', 'Mazurek', '180','niebieskie'); /*stwórz nową instancję obiektu Osoba*/
+var maciej = new Osoba('Maciej', 'Mazurek', '180', 'niebieskie'); /*stwórz nową instancję obiektu Osoba*/
 
-
-
-console.log(maciej);
+maciej.wyswietlInfo(); /*wywołuje funkcje*/
