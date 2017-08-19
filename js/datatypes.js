80870f93d1386d2f8181dd956a928101b077d629
macmaz <maciej.mazurek87@gmail.com>
2017-08-19 10:32:48 +0200

    types of variables

---
+++ js/datatypes.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/* typ liczbowy*/
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata = wyplata + premia;
+
+console.log (calkowitaWyplata);
+
+/* Typ - łańcuch znaków - string*/
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+/* typ logiczny - boolean*/
+
+var smog = true;
+console.log(smog);
+
+/* typy specjalne - udefined / null */
+
+var niezdefiniowanaZmienna;
+var nullZmienna = null;
+
+console.log (niezdefiniowanaZmienna);
+console.log (nullZmienna);
+
