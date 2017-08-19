51560c84f0f03ade87aaad1ac68ceefe21302bd9
macmaz <maciej.mazurek87@gmail.com>
2017-08-19 09:44:07 +0200

    variables 101

---
+++ js/variables.js
@@ -0,0 +1,21 @@
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+console.log(imie);
+console.log(wiek);
+console.log(imieDamskie);
+
+imie = "Maciej";
+imieDamskie = "Zosia";
+
+console.log("*******************");
+console.log(imie);
+console.log(wiek);
+console.log(imieDamskie);
+
+imie = imieDamskie;
+console.log("*******************");
+console.log(imie);
+console.log(wiek);
+console.log(imieDamskie);
