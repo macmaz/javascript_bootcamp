eb43fd8a1ad958d178998224de23d336eefce75f
macmaz <maciej.mazurek87@gmail.com>
2017-08-19 15:35:50 +0200

    conditions 101

---
+++ js/conditions.js
@@ -0,0 +1,17 @@
+var wzrostMateusz = 190;
+var wzrostOlgi = 190;
+
+/* warunek if */
+
+if (wzrostOlgi < wzrostMateusz) {
+    console.log ("Olga jest niższa");
+}
+
+/* warunek if else*/
+
+if (wzrostOlgi < wzrostMateusz) {
+    console.log ("Olga jest niższa");
+}
+else {
+    console.log ("Olga jest wyższa");
+}
---
+++ js/operators.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var liczba1 = 13, liczba2 = 5;
+var wynik;
+
+wynik = liczba1 % liczba2;
+
+console.log(wynik); //3
+
+console.log(wynik++);//3
+
+console.log(wynik);//4
+
+console.log(++wynik);//5
+
+wynik = liczba1 +=8; // liczba1 = liczba1 + 8
+
+var text = "Wojtek";
+text += " Potocki";
+
+console.log(text);
+
+var liczba = 10;
+var text = "10";
+
+console.log(liczba == text); //sprawdza wartości
+
+console.log(liczba === text); //sprawdza typ
+
+
+console.log(liczba1 != liczba2);
+
+
