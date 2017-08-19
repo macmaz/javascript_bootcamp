5059f50c0cdc8df90fa97dd2bf9055aa1079b1f7
macmaz <maciej.mazurek87@gmail.com>
2017-08-19 14:11:56 +0200

    object constructor

--- js/objects.js
+++ js/objects.js
@@ -1,18 +1,44 @@
 'use strict';
 
-var osoba = {
+/*var maciej = {
     imie: "Maciej",
     wzrost: 185,
-    przedstawOsobe: function() {
+    przedstawOsobe: function () {
         console.log("Cześć jestem " + this.imie);
     }
-    
+
 }
 
-console.log(osoba["imie"]);
-osoba.przedstawOsobe()
+console.log(maciej["imie"]);
+maciej.przedstawOsobe()
 
-osoba.imie = "Adam";
-console.log(osoba["imie"]);
+maciej.imie = "Adam";
+console.log(maciej["imie"]);
 
-osoba.przedstawOsobe()
+maciej.przedstawOsobe()
+
+var kaja = {
+    imie: "Kaja",
+    wzrost: 165,
+    przedstawOsobe: function () {
+        console.log("Cześć jestem " + this.imie + " i mam " + this.wzrost + " cm wzrostu:)");
+    }
+
+}
+
+kaja.przedstawOsobe() */
+
+class Osoba {
+    constructor (imie,nazwisko,wzrost, oczy) {
+this.imie = imie;
+this.nazwisko = nazwisko;
+        this.wzrost = wzrost;
+        this.oczy = oczy;
+}
+}
+
+var maciej = new Osoba('Maciej', 'Mazurek', '180','niebieskie'); /*stwórz nową instancję obiektu Osoba*/
+
+
+
+console.log(maciej);
